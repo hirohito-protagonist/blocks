331a35315aabc93f5ef464e6fdad96de74bca12c
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2020-02-28 20:09:36 +0100

    test(game-board): points

---
+++ apps/blocks/src/features/game-board/points.spec.ts
@@ -0,0 +1,19 @@
+import { getLinesClearedPoints } from './points';
+
+describe('points', () => {
+
+  describe('getLinesClearedPoints', () => {
+
+    it('should return 0 by default', () => {
+      // Given
+      const lines = 0;
+      const level = 0;
+
+      // When
+      const result = getLinesClearedPoints(lines, level);
+
+      // Then
+      expect(result).toEqual(0);
+    });
+  });
+});
