0e5f11abd6a09c89af15c702f4d3fb54bb0bc27f
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2020-02-29 20:11:14 +0100

    test(game-board): points

--- apps/blocks/src/features/game-board/points.spec.ts
+++ apps/blocks/src/features/game-board/points.spec.ts
@@ -15,5 +15,17 @@ describe('points', () => {
       // Then
       expect(result).toEqual(0);
     });
+
+    it('should calculate for single line', () => {
+      // Given
+      const lines = 1;
+      const level = 0;
+
+      // When
+      const result = getLinesClearedPoints(lines, level);
+
+      // Then
+      expect(result).toEqual(100);
+    });
   });
 });
