c1010b42b00467895e5ed33350275a8c5034fa1e
HiroHito Protagonist <hirohito.protagonist@protonmail.com>
2023-03-29 17:51:15 +0200

    feat: add keyboard hook

--- src/common/hooks.ts
+++ src/common/hooks.ts
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 export const useGameLoop = (callback: (t: number) => void, deps: any[] = []) => {
 
@@ -17,4 +17,30 @@ export const useGameLoop = (callback: (t: number) => void, deps: any[] = []) => 
       }
     }
   }, deps);
+};
+
+
+export const useKeyboard = (keyTarget: string): boolean => {
+  const [keyPressed, setKeyPressed] = useState(false);
+  const downHandler = ({ key }: KeyboardEvent) => {
+    if (key === keyTarget) {
+      setKeyPressed(true);
+    }
+  }
+  
+  const upHandler = ({ key }: KeyboardEvent) => {
+    if (key === keyTarget) {
+      setKeyPressed(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', downHandler);
+    window.addEventListener('keyup', upHandler);
+    return () => {
+      window.removeEventListener('keydown', downHandler);
+      window.removeEventListener('keyup', upHandler);
+    };
+  }, []);
+  return keyPressed;
 };
