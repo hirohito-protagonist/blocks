df71bdaba1032b15c55b9140fceb590433f4a989
HiroHito Protagonist <hirohito.protagonist@protonmail.com>
2023-04-17 19:43:18 +0200

    feat: block move left

--- src/features/game-piece/block.spec.ts
+++ src/features/game-piece/block.spec.ts
@@ -2,81 +2,95 @@ import { SHAPES } from './shapes';
 import { BlockRecord } from './block';
 
 describe('BlockRecord', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
+  it('should create', () => {
+    const block = new BlockRecord(3, 0, '#A62991', []);
+    expect(block.x).toEqual(3);
+    expect(block.y).toEqual(0);
+    expect(block.color).toEqual('#A62991');
+    expect(block.shape).toEqual([]);
+  });
 
-    it('should create', () => {
-        const block = new BlockRecord(3, 0, '#A62991', []);
-        expect(block.x).toEqual(3);
-        expect(block.y).toEqual(0);
-        expect(block.color).toEqual('#A62991');
-        expect(block.shape).toEqual([]);
-    });
+  it('should create with random shape', () => {
+    const randomSpy = jest.spyOn(Math, 'random').mockReturnValue(0.99);
+    const block = BlockRecord.withRandomShape();
+    expect(block.x).toEqual(3);
+    expect(block.y).toEqual(0);
+    expect(block.color).toEqual('#A62991');
+    expect(block.shape).toEqual(SHAPES[7]);
+    expect(randomSpy).toHaveBeenCalledTimes(1);
+  });
 
-    it('should create with random shape', () => {
-        const randomSpy = jest.spyOn(Math, 'random').mockReturnValue(0.99);
-        const block = BlockRecord.withRandomShape();
-        expect(block.x).toEqual(3);
-        expect(block.y).toEqual(0);
-        expect(block.color).toEqual('#A62991');
-        expect(block.shape).toEqual(SHAPES[7]);
-        expect(randomSpy).toHaveBeenCalledTimes(1);
-    });
+  it('should rotate', () => {
+    const shape = [
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0],
+    ];
+    const block = new BlockRecord(0, 0, '#A62991', shape);
+    const rotate1 = block.rotate();
+    const rotate2 = rotate1.rotate();
+    const rotate3 = rotate2.rotate();
+    const rotate4 = rotate3.rotate();
+    const rotate5 = rotate4.rotate();
 
-    it('should rotate', () => {
-        const shape = [
-            [0, 1, 0],
-            [1, 1, 1],
-            [0, 0, 0]
-        ];
-        const block = new BlockRecord(0, 0, '#A62991', shape);
-        const rotate1 = block.rotate();
-        const rotate2 = rotate1.rotate();
-        const rotate3 = rotate2.rotate();
-        const rotate4 = rotate3.rotate();
-        const rotate5 = rotate4.rotate();
+    expect(rotate1.shape).toEqual([
+      [0, 1, 0],
+      [0, 1, 1],
+      [0, 1, 0],
+    ]);
+    expect(rotate2.shape).toEqual([
+      [0, 0, 0],
+      [1, 1, 1],
+      [0, 1, 0],
+    ]);
+    expect(rotate3.shape).toEqual([
+      [0, 1, 0],
+      [1, 1, 0],
+      [0, 1, 0],
+    ]);
+    expect(rotate4.shape).toEqual([
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0],
+    ]);
+    expect(rotate5.shape).toEqual([
+      [0, 1, 0],
+      [0, 1, 1],
+      [0, 1, 0],
+    ]);
+  });
 
-        expect(rotate1.shape).toEqual([
-            [0, 1, 0],
-            [0, 1, 1],
-            [0, 1, 0]
-        ]);
-        expect(rotate2.shape).toEqual([
-            [0, 0, 0],
-            [1, 1, 1],
-            [0, 1, 0]
-        ]);
-        expect(rotate3.shape).toEqual([
-            [0, 1, 0],
-            [1, 1, 0],
-            [0, 1, 0]
-        ]);
-        expect(rotate4.shape).toEqual([
-            [0, 1, 0],
-            [1, 1, 1],
-            [0, 0, 0]
-        ]);
-        expect(rotate5.shape).toEqual([
-            [0, 1, 0],
-            [0, 1, 1],
-            [0, 1, 0]
-        ]);
-    });
+  it('should move down', () => {
+    const block = new BlockRecord(3, 0, '#A62991', []);
+    const moved = block.moveDown();
 
-    it('should move down', () => {
-        const block = new BlockRecord(3, 0, '#A62991', []);
-        const moved = block.moveDown();
+    expect(block.x).toEqual(3);
+    expect(block.y).toEqual(0);
+    expect(block.color).toEqual('#A62991');
+    expect(block.shape).toEqual([]);
 
-        expect(block.x).toEqual(3);
-        expect(block.y).toEqual(0);
-        expect(block.color).toEqual('#A62991');
-        expect(block.shape).toEqual([]);
+    expect(moved.x).toEqual(3);
+    expect(moved.y).toEqual(1);
+    expect(moved.color).toEqual('#A62991');
+    expect(moved.shape).toEqual([]);
+  });
 
-        expect(moved.x).toEqual(3); 
-        expect(moved.y).toEqual(1);
-        expect(moved.color).toEqual('#A62991');
-        expect(moved.shape).toEqual([]);
-    });
+  it('should move left', () => {
+    const block = new BlockRecord(3, 0, '#A62991', []);
+    const moved = block.moveLeft();
+
+    expect(block.x).toEqual(3);
+    expect(block.y).toEqual(0);
+    expect(block.color).toEqual('#A62991');
+    expect(block.shape).toEqual([]);
+
+    expect(moved.x).toEqual(2);
+    expect(moved.y).toEqual(0);
+    expect(moved.color).toEqual('#A62991');
+    expect(moved.shape).toEqual([]);
+  });
 });
--- src/features/game-piece/block.ts
+++ src/features/game-piece/block.ts
@@ -1,31 +1,60 @@
 import { SHAPES } from './shapes';
 
 const randomizeTetrominoType = (noOfTypes: number): number => {
-    return Math.floor(Math.random() * noOfTypes + 1);
-}
+  return Math.floor(Math.random() * noOfTypes + 1);
+};
 
 export class BlockRecord {
-    constructor(public x: number, public y: number, public color: string, public shape: number[][]) {}
+  constructor(
+    public x: number,
+    public y: number,
+    public color: string,
+    public shape: number[][]
+  ) {}
 
-    rotate(): BlockRecord {
-        const shape = this.copyArray(this.shape);
-        for (let y = 0; y < shape.length; ++y) {
-            for (let x = 0; x < y; ++x) {
-              [shape[x][y], shape[y][x]] = [shape[y][x], shape[x][y]];
-            }
-        }
-        return new BlockRecord(this.x, this.x, this.color, shape.map(row => row.reverse()));
+  rotate(): BlockRecord {
+    const shape = this.copyArray(this.shape);
+    for (let y = 0; y < shape.length; ++y) {
+      for (let x = 0; x < y; ++x) {
+        [shape[x][y], shape[y][x]] = [shape[y][x], shape[x][y]];
+      }
     }
+    return new BlockRecord(
+      this.x,
+      this.x,
+      this.color,
+      shape.map((row) => row.reverse())
+    );
+  }
 
-    moveDown(): BlockRecord {
-        return new BlockRecord(this.x, this.y + 1, this.color, this.copyArray(this.shape));
-    }
+  moveDown(): BlockRecord {
+    return new BlockRecord(
+      this.x,
+      this.y + 1,
+      this.color,
+      this.copyArray(this.shape)
+    );
+  }
 
-    private copyArray(arr: number[][]): number[][] {
-        return arr.map(v => v.map(d => d));
-    }
+  moveLeft(): BlockRecord {
+    return new BlockRecord(
+      this.x - 1,
+      this.y,
+      this.color,
+      this.copyArray(this.shape)
+    );
+  }
 
-    static withRandomShape(): BlockRecord {
-        return new BlockRecord(3, 0, '#A62991', SHAPES[randomizeTetrominoType(SHAPES.length - 1)]);
-    }
+  private copyArray(arr: number[][]): number[][] {
+    return arr.map((v) => v.map((d) => d));
+  }
+
+  static withRandomShape(): BlockRecord {
+    return new BlockRecord(
+      3,
+      0,
+      '#A62991',
+      SHAPES[randomizeTetrominoType(SHAPES.length - 1)]
+    );
+  }
 }
