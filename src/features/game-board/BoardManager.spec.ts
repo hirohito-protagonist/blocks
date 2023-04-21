a47aacd487912f8f0ba677a9dd9c329181152589
HiroHito Protagonist <hirohito.protagonist@protonmail.com>
2023-04-21 18:31:37 +0200

    test: collision in board

--- src/features/game-board/BoardManager.spec.ts
+++ src/features/game-board/BoardManager.spec.ts
@@ -1,27 +1,45 @@
 import { BoardManager } from './BoardManager';
 
 describe('BoardManager', () => {
+  describe('isNotInCollision', () => {
+    it('should return true when block is in board and touch anything', () => {
+      // Given
+      const block = {
+        shape: [
+          [0, 1, 0],
+          [1, 1, 1],
+          [0, 0, 0],
+        ],
+        x: 1,
+        y: 2,
+      };
+      const boardManager = BoardManager.createEmpty(7, 4);
 
-    describe('isNotInCollision', () => {
+      // When
+      const result = boardManager.isNotInCollision(block);
 
-        it('should return true when block is in board and touch anything', () => {
-            // Given
-            const block = {
-              shape: [
-                [0, 1, 0],
-                [1, 1, 1],
-                [0, 0, 0]
-              ],
-              x: 1,
-              y: 2
-            };
-            const boardManager = BoardManager.createEmpty(7, 4);
-        
-            // When
-            const result = boardManager.isNotInCollision(block);
-        
-            // Then
-            expect(result).toEqual(true);
-          });
+      // Then
+      expect(result).toEqual(true);
     });
+
+    it('should return false when piece is touch wall', () => {
+      // Given
+      const block = {
+        shape: [
+          [0, 1, 0],
+          [1, 1, 1],
+          [0, 0, 0],
+        ],
+        x: -1,
+        y: 2,
+      };
+      const boardManager = BoardManager.createEmpty(7, 4);
+
+      // When
+      const result = boardManager.isNotInCollision(block);
+
+      // Then
+      expect(result).toEqual(false);
+    });
+  });
 });
