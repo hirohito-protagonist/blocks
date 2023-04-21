7671f8442e035239164261e587542327a376c2c0
HiroHito Protagonist <hirohito.protagonist@protonmail.com>
2023-04-21 18:27:07 +0200

    test: collision for board

---
+++ src/features/game-board/BoardManager.spec.ts
@@ -0,0 +1,27 @@
+import { BoardManager } from './BoardManager';
+
+describe('BoardManager', () => {
+
+    describe('isNotInCollision', () => {
+
+        it('should return true when block is in board and touch anything', () => {
+            // Given
+            const block = {
+              shape: [
+                [0, 1, 0],
+                [1, 1, 1],
+                [0, 0, 0]
+              ],
+              x: 1,
+              y: 2
+            };
+            const boardManager = BoardManager.createEmpty(7, 4);
+        
+            // When
+            const result = boardManager.isNotInCollision(block);
+        
+            // Then
+            expect(result).toEqual(true);
+          });
+    });
+});
