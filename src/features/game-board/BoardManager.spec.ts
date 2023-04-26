ecb32a727e6141253e2bed8315144cd0435fba53
HiroHito Protagonist <hirohito.protagonist@protonmail.com>
2023-04-26 21:42:59 +0200

    feat: check collision with other blocks

--- src/features/game-board/BoardManager.spec.ts
+++ src/features/game-board/BoardManager.spec.ts
@@ -41,5 +41,67 @@ describe('BoardManager', () => {
       // Then
       expect(result).toEqual(false);
     });
+
+    it('should return false when block touches other blocks', () => {
+      // Given
+      const block = {
+        shape: [
+          [0, 1, 0],
+          [1, 1, 1],
+          [0, 0, 0],
+        ],
+        x: 1,
+        y: 2,
+      };
+      const boardManager = BoardManager.createEmpty(7, 4);
+      boardManager.freeze({
+        shape: [
+          [0, 1, 0],
+          [1, 1, 1],
+          [0, 0, 0],
+        ],
+        x: 0,
+        y: 2,
+      });
+      boardManager.freeze({
+        shape: [
+          [0, 1, 0],
+          [1, 1, 1],
+          [0, 0, 0],
+        ],
+        x: 1,
+        y: 2,
+      });
+      boardManager.freeze({
+        shape: [
+          [0, 1, 0],
+          [1, 1, 1],
+          [0, 0, 0],
+        ],
+        x: 3,
+        y: 2,
+      });
+      boardManager.freeze({
+        shape: [
+          [0, 1, 0],
+          [1, 1, 1],
+          [0, 0, 0],
+        ],
+        x: 4,
+        y: 2,
+      });
+
+      // When
+      const result = boardManager.isNotInCollision(block);
+
+      // Then
+      expect(result).toEqual(false);
+      expect(boardManager.getBoard()).toEqual([
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 1, 1, 0],
+        [1, 1, 1, 1, 1, 1, 1],
+      ]);
+    });
   });
 });
